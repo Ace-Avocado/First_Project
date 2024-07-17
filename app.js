cfbc12258acd04e1bbe86ead05527ae0f7658f44
Mitchell <Mitchell.corbett@yahoo.com>
2024-07-17 10:45:43 -0400

    commit added

---
+++ app.js
@@ -0,0 +1,46 @@
+
+
+let cursedT = Math.floor(Math.random()*10 + 1)
+
+switch(cursedT){
+
+    case 1:
+        console.log("Straw Doll");
+        break;
+        case 2:
+            console.log("10 Shadows");
+            break;
+            case 3:
+                console.log("Cursed Speech");
+                break;
+                case 4:
+        console.log("Jaccobs Ladder");
+        break;
+        case 5:
+            console.log("Shrine");
+            break;
+            case 6:
+                console.log("Limitless");
+                break;
+                case 7:
+        console.log("Ratio");
+        break;
+        case 8:
+            console.log("Idle Transfiguration");
+            break;
+            case 9:
+                console.log("Boogie Woogie");
+                break;
+                case 10:
+        console.log("Blood Mantuplation");
+        break;
+        case 2:
+            console.log("10 Shadow Technique");
+            
+
+
+
+}
+
+
+
